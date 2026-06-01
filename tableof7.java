5899ce298e3f7b65a077957a365dcc5847f42455
adi9007 <atharvaraiadi78@gmail.com>
2026-06-01 14:51:58 +0530

    Add files via upload

---
+++ tableof7.java
@@ -0,0 +1,8 @@
+public class tableof7 {
+    public static void main(String[] args) {
+        int number = 7;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
